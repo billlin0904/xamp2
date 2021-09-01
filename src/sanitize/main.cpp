5b4b363bf341beae22f71e874f0d68eef81b1d51
rdbill0452 <rdbill0452@RD1-BILLPC>
2021-09-01 18:24:12 +0800

    backup

---
+++ src/sanitize/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <base/scopeguard.h>
+#include <output_device/asiodevicetype.h>
+#include <player/audio_player.h>
+
+int main()
+{
+	using namespace xamp;
+	using namespace xamp::player;
+	using namespace xamp::base;
+	using namespace xamp::output_device;
+
+	Logger::GetInstance()
+		.AddDebugOutputLogger()
+		.AddFileLogger("xamp.log")
+		.GetLogger("xamp");
+
+	XAMP_ON_SCOPE_EXIT(
+		Logger::GetInstance().Shutdown();
+	);
+
+	AudioPlayer::LoadDecoder();
+
+	auto player = std::make_shared<AudioPlayer>();
+	player->Open("C:\\Users\\rdbill0452\\Music\\Test\\DSD.dsf", ASIODeviceType::Id);
+	player->PrepareToPlay();
+	player->Play();
+	std::cin.get();
+	player->Stop();
+}
