fff887ec84d7eb1ba5988df38d183ae3c66daeac
billlin0904 <billlin0904@gmail.com>
2026-05-21 16:06:43 +0800

    Clean unused release artifacts

--- src/thirdparty/libebur128/msvc/ebur128/queuetest.c
+++
@@ -1,3 +0,0 @@
-#include <sys/queue.h>
-LIST_HEAD(listhead, entry) head;
-int main() { return 0; }
--- src/thirdparty/supereq/supereq.cpp
+++
@@ -1,5 +0,0 @@
-#include "supereq.h"
-
-std::unique_ptr<supereq_base> make_supereq() {
-	return std::make_unique<supereq<float>>();
-}
